beb2310578f8004f19ef7ab4e10b4e3701985164
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-11-05 02:37:08 +0530

    Added api

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import cv2
+import numpy as np
+from scipy.misc import imresize
 from sklearn.neural_network import MLPClassifier
 from sklearn.externals import joblib
 from mnist import MNIST
@@ -15,23 +19,37 @@ def load_data():
     test_data = mndata.load_testing()
     return train_data, test_data
 
-def train_nn(train_data, iterations=200, n_neurons=4000, dump=True):
+def train_nn(train_data, iterations=1000, n_neurons=400, dump=True):
     """To Train the Neural Network
     
     Args:
         Training Data
-        Iterations (Default = 200)
-        Number of Neurons in Hidden Layer (Default = 4000)
+        Iterations (Default = 1000)
+        Number of Neurons in Hidden Layer (Default = 400)
 
     Returns:
         Trained Neural Network"""
     clf = MLPClassifier(hidden_layer_sizes=(n_neurons,),
+                        activation='logistic',
                         random_state=1,
                         max_iter=iterations)
     clf.fit(train_data[0], train_data[1]);
     if dump:
         joblib.dump(clf, 'trained_models/nn_iter' + str(iterations) +
                          '_neurons_' + str(n_neurons) + '.model')
+    return clf
+
+def load_trained_nn(iterations=1000, n_neurons=400):
+    """To Load an Already Trained Neural Network
+    
+    Args:
+        Number of Iterations
+        Number of Neurons
+        
+    Returns:
+        Trained Neural Network or None if not found"""
+    clf = joblib.load('trained_models/nn_iter' + str(iterations) +
+                       '_neurons_' + str(n_neurons) + '.model')
     return clf
 
 def compute_score(classifier, test_data):
@@ -59,14 +77,26 @@ def main():
         
     Returns:
         None"""
+
     train_data, test_data = load_data()
-    for n_neurons in range(100, 1001, 100):
-        print 'Training Starts'
-        clf = train_nn(train_data=train_data, n_neurons=n_neurons)
-        print 'Training Done'
-        print 'N Neurons =', n_neurons
-        print 'Score:', compute_score(clf, test_data)
-    
+    if len(sys.argv) == 1:
+        for n_neurons in range(100, 1001, 100):
+            clf = train_nn(train_data=train_data, n_neurons=n_neurons)
+            print 'Number of Iterations =', clf.n_iter_
+            print 'N Neurons =', n_neurons
+            print 'Score:', compute_score(clf, test_data)
+    elif len(sys.argv) == 4 and sys.argv[1] == 'example':
+        clf = load_trained_nn(n_neurons=sys.argv[2])
+        orig_img = test_data[0][int(sys.argv[3])]
+        img = np.array(orig_img)
+        img = np.reshape(img, (28,28))
+        img = img/255.0
+        img = imresize(img, (280, 280))
+        cv2.imshow('Show', img)
+        cv2.waitKey(0)
+        print 'Predicted Digit:', clf.predict([orig_img])[0]
+    else:
+        print 'Error'
 
 if __name__ == '__main__':
     main()
